e526df3c3ce82e0931c7103bdbb49dd2bc3ef66b
Aboodbr <aboodragon12345@gmail.com>
2025-02-17 01:47:00 +0200

    register_view.dart
    add account

--- lib/presentation/register/register_view.dart
+++ lib/presentation/register/register_view.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:maternity_app/presentation/Questions/q1.dart';
@@ -23,6 +24,35 @@ class _RegisterViewState extends State<RegisterView> {
 
   bool _isPasswordVisible = false;
 
+  Future<void> _registerUser() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      try {
+        // تسجيل المستخدم في Firebase
+        UserCredential userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+
+        // التحقق من نجاح التسجيل
+        if (userCredential.user != null) {
+          print('User registered successfully: ${userCredential.user!.email}');
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => Q1()),
+          );
+        }
+      } catch (e) {
+        print('Error registering user: $e');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Registration failed: $e')),
+        );
+      }
+    } else {
+      print('Form is invalid!');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
@@ -30,10 +60,9 @@ class _RegisterViewState extends State<RegisterView> {
 
     return Scaffold(
       body: Container(
-        // Background Image
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/Sign_Up.png'), // Replace with your image path
+            image: AssetImage('assets/images/Sign_Up.png'),
             fit: BoxFit.cover,
           ),
         ),
@@ -41,63 +70,50 @@ class _RegisterViewState extends State<RegisterView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // AppBar with Back Arrow, Logo, and Title
               AppBar(
-                backgroundColor: Colors.transparent, // Transparent AppBar
-                elevation: 0, // Remove shadow
-                centerTitle: true, // Center the title content
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                centerTitle: true,
                 leading: GestureDetector(
                   onTap: () {
                     Navigator.pop(context);
                   },
-                  child: const Icon(
-                    Icons.arrow_back_ios_new_sharp,
-                    color: Colors.black,
-                  ),
+                  child: const Icon(Icons.arrow_back_ios_new_sharp,
+                      color: Colors.black),
                 ),
                 title: Row(
-                  mainAxisSize: MainAxisSize.min, // Use minimum space for the row content
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Image.asset(
-                      'assets/images/logo2.png', // Replace with your logo path
-                      height: screenHeight * 0.08,
-                      width: screenWidth * 0.1,
-                    ),
+                    Image.asset('assets/images/logo2.png',
+                        height: screenHeight * 0.08, width: screenWidth * 0.1),
                     SizedBox(width: screenWidth * 0.02),
                     Text(
                       'Mamativity',
                       style: GoogleFonts.inriaSerif(
                         textStyle: TextStyle(
-                          fontSize: screenWidth * 0.05,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
+                            fontSize: screenWidth * 0.05,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
                       ),
                     ),
                   ],
                 ),
               ),
-
-              // Title: Create Account
               Padding(
                 padding: EdgeInsets.symmetric(
-                  horizontal: screenWidth * 0.08,
-                  vertical: screenWidth * 0.1,
-                ),
+                    horizontal: screenWidth * 0.08,
+                    vertical: screenWidth * 0.1),
                 child: Text(
                   'Create\nAccount',
                   style: GoogleFonts.inriaSerif(
                     textStyle: TextStyle(
-                      fontSize: screenWidth * 0.10,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+                        fontSize: screenWidth * 0.10,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
                 ),
               ),
               SizedBox(height: screenHeight * 0.02),
-
-              // Form Section
               Expanded(
                 child: Container(
                   padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
@@ -107,81 +123,22 @@ class _RegisterViewState extends State<RegisterView> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          // First Name and Last Name
-                          Row(
-                            children: [
-                              Expanded(
-                                child: TextFormField(
-                                  controller: _firstNameController,
-                                  decoration: InputDecoration(
-                                    labelText: 'First Name',
-                                    labelStyle: GoogleFonts.inriaSerif(
-                                      textStyle: TextStyle(
-                                        fontSize: screenWidth * 0.04,
-                                        color: ColorManager.txtEditor_font_color,
-                                      ),
-                                    ),
-                                  ),
-                                  validator: (value) =>
-                                      InputValidator.validateName(value),
-                                ),
-                              ),
-                              SizedBox(width: screenWidth * 0.04),
-                              Expanded(
-                                child: TextFormField(
-                                  controller: _lastNameController,
-                                  decoration: InputDecoration(
-                                    labelText: 'Last Name',
-                                    labelStyle: GoogleFonts.inriaSerif(
-                                      textStyle: TextStyle(
-                                        fontSize: screenWidth * 0.04,
-                                        color: ColorManager.txtEditor_font_color,
-                                      ),
-                                    ),
-                                  ),
-                                  validator: (value) =>
-                                      InputValidator.validateName(value),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: screenHeight * 0.02),
-
-                          // Email Field
                           TextFormField(
                             controller: _emailController,
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              labelStyle: GoogleFonts.inriaSerif(
-                                textStyle: TextStyle(
-                                  fontSize: screenWidth * 0.04,
-                                  color: ColorManager.txtEditor_font_color,
-                                ),
-                              ),
-                            ),
+                            decoration: InputDecoration(labelText: 'Email'),
                             validator: (value) =>
                                 InputValidator.validateEmail(value),
                           ),
                           SizedBox(height: screenHeight * 0.02),
-
-                          // Password Field with Visibility Toggle
                           TextFormField(
                             controller: _passwordController,
                             obscureText: !_isPasswordVisible,
                             decoration: InputDecoration(
                               labelText: 'Password',
-                              labelStyle: GoogleFonts.inriaSerif(
-                                textStyle: TextStyle(
-                                  fontSize: screenWidth * 0.04,
-                                  color: ColorManager.txtEditor_font_color,
-                                ),
-                              ),
                               suffixIcon: IconButton(
-                                icon: Icon(
-                                  _isPasswordVisible
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
+                                icon: Icon(_isPasswordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
                                 onPressed: () {
                                   setState(() {
                                     _isPasswordVisible = !_isPasswordVisible;
@@ -193,101 +150,42 @@ class _RegisterViewState extends State<RegisterView> {
                                 InputValidator.validatePassword(value),
                           ),
                           SizedBox(height: screenHeight * 0.02),
-
-                          // Phone Field
-                          TextFormField(
-                            controller: _phoneController,
-                            decoration: InputDecoration(
-                              labelText: 'Phone',
-                              labelStyle: GoogleFonts.inriaSerif(
-                                textStyle: TextStyle(
-                                  fontSize: screenWidth * 0.04,
-                                  color: ColorManager.txtEditor_font_color,
+                          GestureDetector(
+                            onTap: _registerUser,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xFFB6E8F8),
+                                    Color(0xFF90CAF9)
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
                                 ),
                               ),
+                              padding: EdgeInsets.all(screenWidth * 0.06),
+                              child: Icon(Icons.arrow_forward,
+                                  color: Colors.black,
+                                  size: screenWidth * 0.06),
                             ),
-                            validator: (value) =>
-                                InputValidator.validatePhoneNumber(value),
                           ),
-                          SizedBox(height: screenHeight * 0.04),
-
-                          // Sign-Up Button (Circular Arrow)
-                          Row(
-                            children: [
-                              // "Sign up" Text
-                              Text(
-                                'Sign up',
-                                textAlign: TextAlign.start,
-                                style: GoogleFonts.inriaSerif(
-                                  textStyle: TextStyle(
-                                    fontSize: screenWidth * 0.06,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                              const Spacer(),
-
-                              // Circular Button
-                              GestureDetector(
-                                onTap: () {
-                                  if (_formKey.currentState?.validate() ??
-                                      false) {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => Q1(),
-                                      ),
-                                    );
-                                    print('Form is valid!');
-                                  } else {
-                                    print('Form is invalid!');
-                                  }
-                                },
-                                child: Container(
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    gradient: LinearGradient(
-                                      colors: [
-                                        Color(0xFFB6E8F8), // Light Blue
-                                        Color(0xFF90CAF9), // Sky Blue
-                                      ],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight,
-                                    ),
-                                  ),
-                                  padding: EdgeInsets.all(screenWidth * 0.06),
-                                  child: Icon(
-                                    Icons.arrow_forward,
-                                    color: Colors.black,
-                                    size: screenWidth * 0.06,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-
                           SizedBox(height: screenHeight * 0.07),
-
-                          // Sign-In Link
                           GestureDetector(
                             onTap: () {
                               Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const LoginView(),
-                                ),
-                              );
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const LoginView()));
                             },
                             child: Text(
                               'Sign In',
                               style: GoogleFonts.inriaSerif(
                                 textStyle: TextStyle(
-                                  fontSize: screenWidth * 0.04,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                  decoration: TextDecoration.underline,
-                                ),
+                                    fontSize: screenWidth * 0.04,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    decoration: TextDecoration.underline),
                               ),
                             ),
                           ),
